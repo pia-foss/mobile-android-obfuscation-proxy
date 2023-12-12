7b4e1f75240b3145755db7f9fb9e3115d9c96dff
Juan Docal <109512072+kp-juan-docal@users.noreply.github.com>
2023-12-12 14:41:13 +0100

    PIA-1022: Bootstrap project structure (#1)

---
+++ testapp/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.kape.testapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.kape.testapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(project(":obfuscator"))
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+}
